bcbc9ec7e56318e5c5e512786e72f685e057df32
skr47ch_g4m3z <its.me.arrived@gmail.com>
2017-02-22 03:57:32 +0530

    Working on Logic_Detective

--- codewars_java/src/main/java/Merged_String_Checker/Main.java
+++ codewars_java/src/main/java/Merged_String_Checker/Main.java
@@ -2,7 +2,10 @@ package Merged_String_Checker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(StringMerger.isMerge("ddfhhggttpp", "dfggtp", "dhhtp"));
+        //System.out.println(StringMerger.isMerge("3/<!.^j^t\\qq2mH&BsTPDbB", "3/<^\\qBsTPD", "!.^jtq2mH&bB"));
+        System.out.println(StringMerger.isMerge("adcctj", "act", "dcj"));
+
+        System.out.println("\\".length() + " " + ",".length());
     }
 }
 
--- codewars_java/src/main/java/Merged_String_Checker/StringMerger.java
+++ codewars_java/src/main/java/Merged_String_Checker/StringMerger.java
@@ -5,42 +5,83 @@ import java.util.List;
 
 public class StringMerger {
     public static boolean isMerge(String s, String part1, String part2) {
-        List<String> listMain = new ArrayList<String>();
-        List<String> list1 = new ArrayList<String>();
-        List<String> list2 = new ArrayList<String>();
         int m = 0;
+        int x = 0;
+        int y = 0;
+        int pos = 0;
+        List<String> test = new ArrayList<String>();
+
+        int xom = part1.length() + part2.length();
+
+        System.out.println(s.length() + " " + part1.length() + " " + part2.length() + " " + xom);
 
         if (s.length() != part1.length() + part2.length())
             return  false;
 
-        if (part1.length() == 0 && part2.equals(s))
-            return true;
-        else if (part2.length() == 0 && part1.equals(s))
-            return true;
-
-        for (int i = 0; i < part1.length() && i < part2.length();) {
-            int k = i+1;
-            if (part1.charAt(i) == part2.charAt(i)) {
-                for (; k < part1.length() && k < part2.length(); k++) {
-                    if (part1.charAt(k) != part2.charAt(k)) {
-                        k += 1;
-                        break;
-                    }
-                }
-            }
-            list1.add(part1.substring(i, k));
-            list2.add(part2.substring(i, k));
-
-            //m += i;
-            listMain.add(s.substring(m, m + k));
-            listMain.add(s.substring(m + k, m + k + k ));
-
-            i = k;
+        if (part1.length() == 0) {
+            if(part2.equals(s)) return  true;
+            else  return false;
         }
 
-        System.out.println(listMain);
-        System.out.println(list1);
-        System.out.println(list2);
-        return  false;
+        if (part2.length() == 0) {
+            if(part1.equals(s)) return  true;
+            else  return false;
+        }
+
+        s = s.replace("\\", ",");
+        part1 = part1.replace("\\", ",");
+        part2 = part2.replace("\\", ",");
+
+        for (int i = 0; i < s.length(); i ++) {
+            pos = i;
+
+            System.out.print("  " + i + " " + m + " " + x + " " + y);
+            if (s.charAt(i) == part1.charAt(x)) {
+                if (s.charAt(i) == part2.charAt(y)) {
+                    x++;
+                    y++;
+                    m++;
+                    test.add(Character.toString(s.charAt(i)));
+                    System.out.println(test);
+                } else {
+                    if (m > 0) {
+                        y -= m;
+                        m = 0;
+                    }
+                    x++;
+                    test.add(Character.toString(s.charAt(i)));
+                    System.out.println(test);
+                }
+            } else if (s.charAt(i) == part2.charAt(y)) {
+                if (m > 0) {
+                    x -= m;
+                    m = 0;
+                }
+                y ++;
+                test.add(Character.toString(s.charAt(i)));
+                System.out.println(test);
+            } else return false;
+
+
+
+            if ( x >= part1.length() || y >= part2.length()) {
+                //pos ++;
+                break;
+            }
+        }
+        System.out.println(test);
+        System.out.print("    " + pos  + " " + m + " " + x + " " + y);
+
+
+        if (x >= part1.length()) {
+            if (!s.substring(pos + 1, s.length()).equals(part2.substring(y, part2.length())))
+                return false;
+        }
+
+        if (y >= part2.length()) {
+            if (!s.substring(pos + 1, s.length()).equals(part1.substring(x, part1.length())))
+                return false;
+        }
+        return true;
     }
 }
